ca36f5a2739db9a46a5cb672074084664410ce8c
ebrucihan <ebrchnnn123@gmail.com>
2024-04-30 16:16:25 +0300

    recursive metot oluşturuldu

---
+++ src/Week2_/RecursiveDesenMetot.java
@@ -0,0 +1,28 @@
+package Week2_;
+
+import java.util.Scanner;
+
+public class RecursiveDesenMetot {
+    public static void RecursiveMethod(int n) {
+        // Eğer n 0 veya negatifse, sadece n'i yazdırma
+        if (n <= 0) {
+            System.out.print(n + " ");
+            return;
+        }
+
+        System.out.print(n + " ");
+        // 5 çıkarma işlemi yaparak RecursiveMethod'i tekrar çağırma
+        RecursiveMethod(n - 5);
+        // Orijinal sayıyı tekrar yazdırma
+        System.out.print(n + " ");
+
+    }
+    public static void main(String[] args) {
+        // Kullanıcıdan bir tamsayı almak için Scanner sınıfını kullanma
+        Scanner sc = new Scanner(System.in);
+        System.out.print("N değeri : ");
+        int n = sc.nextInt();
+        RecursiveMethod(n);
+
+    }
+}
